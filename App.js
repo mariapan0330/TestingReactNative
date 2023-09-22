03469fc7350d25868a9e751710b2cf09b8bb7f92
Maria Panagos <maria.pan0330@gmail.com>
2023-09-22 16:21:58 -0500

    playing around with positioning

--- App.js
+++ App.js
@@ -1,20 +1,76 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  Platform,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  Alert,
+  TouchableWithoutFeedback,
+  TouchableOpacity,
+  TouchableHighlight,
+  TouchableNativeFeedback,
+  View,
+  Image,
+  Button,
+  Dimensions,
+} from "react-native";
 
 export default function App() {
+  // console.log("hello world!!");
+  // console.log(Dimensions.get("screen"));
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View
+        style={{
+          backgroundColor: "teal",
+          width: 100,
+          height: 100,
+        }}
+      ></View>
+      <View
+        style={{
+          backgroundColor: "indianred",
+          width: 100,
+          height: 100,
+          right: 20,
+          // position: "absolute",
+        }}
+      />
+      <View
+        style={{
+          backgroundColor: "gold",
+          width: 100,
+          height: 100,
+        }}
+      />
+      {/* <View
+        style={{
+          backgroundColor: "chocolate",
+          width: 100,
+          height: 100,
+        }}
+      />
+      <View
+        style={{
+          backgroundColor: "linen",
+          width: 100,
+          height: 100,
+        }}
+      /> */}
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "darkseagreen",
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+    // flexWrap: "wrap", // flex wrap affects alignitems property because now they're being aligned within EACH line (and when you wrap them, there's gonna be multiple lines)
+    alignContent: "center", // unaffected by flexwrap because it aligns all of the content regardless of lines
+    paddingTop: Platform.OS === "android" ? "10%" : 0,
   },
 });
